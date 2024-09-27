9d88749bf3279fe236ebe0a81e9fcacb22170510
Jouri Fledderman <jouri.fledderman@luminis.eu>
2024-09-27 13:47:58 +0200

    remove unused bus actuator endpoint

--- src/main/java/nl/fledderman/ConfigRefresher.java
+++ src/main/java/nl/fledderman/ConfigRefresher.java
@@ -6,7 +6,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 
 @Component
 public class ConfigRefresher {
